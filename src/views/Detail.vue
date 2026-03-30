36eadf069c5a973e8641fc280db0c570308d545b
Aliyah <emailkamu@example.com>
2026-03-30 15:28:19 +0700

    hi

---
+++ src/views/Detail.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-if="character">
+    <h1>{{ character.name }}</h1>
+    <img :src="character.imageUrl" width="200" />
+
+    <p><b>Films:</b> {{ character.films.join(', ') }}</p>
+    <p><b>TV Shows:</b> {{ character.tvShows.join(', ') }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      character: null
+    }
+  },
+  mounted() {
+    const id = this.$route.params.id
+
+    fetch(`https://api.disneyapi.dev/character/${id}`)
+      .then(res => res.json())
+      .then(data => {
+        this.character = data.data
+      })
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Disney Characters</h1>
+
+    <div v-for="char in characters" :key="char._id">
+      <h3>{{ char.name }}</h3>
+      <img :src="char.imageUrl" width="100" />
+
+      <button @click="goDetail(char._id)">
+        Detail
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      characters: []
+    }
+  },
+  mounted() {
+    fetch('https://api.disneyapi.dev/character')
+      .then(res => res.json())
+      .then(data => {
+        this.characters = data.data
+      })
+  },
+  methods: {
+    goDetail(id) {
+      this.$router.push(`/detail/${id}`)
+    }
+  }
+}
+</script>
